92e5b5ccef66fa615453eae14fc6ea2f68b65432
02daDeepweb <henrylucasantunes@gmail.com>
2025-03-11 11:50:34 -0300

    Feat: att

--- testes/interface.ts
+++ testes/interface.ts
@@ -1,9 +1,9 @@
 interface People {
-  name:      string;
-  age:        number;
-  city:       string;
-  country:    string;
-  married?:   boolean; 
+  name: string;
+  age: number;
+  city: string;
+  country: string;
+  married?: boolean;
   // se colocarmos | entre duas opções, como "sim | não",o sistema só vai aceitar uma delas
   // ex: married?: "sim" | "não"; somente um desse será aceito
   profission: string;
@@ -12,17 +12,17 @@ interface People {
 let CLT: People;
 
 CLT = {
-  name:       'Henry',
-  age:        17,
-  city:       'São Paulo',
-  country:    'SP',
-  profission: 'Desenvolvedor'
+  name: "Henry",
+  age: 17,
+  city: "São Paulo",
+  country: "SP",
+  profission: "Desenvolvedor",
 };
 
 const apresentar = (CLT: People): void => {
   console.log(`Ola me chamo ${CLT.name}`);
   console.log(`Tenho ${CLT.age} anos`);
-  console.log(`Sou ${CLT.profission}`); 
+  console.log(`Sou ${CLT.profission}`);
 };
 
 apresentar(CLT);
